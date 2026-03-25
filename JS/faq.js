0bc3f9158283bad15bdc1c6a566ae55f32b9e128
Husay <reamaeroyandoyan445@gmail.com>
2026-03-25 19:04:11 +0800

    Delete JS directory

--- JS/faq.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.faq-q').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const item = btn.parentElement;
-      const isOpen = item.classList.contains('open');
-      document.querySelectorAll('.faq-item').forEach(i => i.classList.remove('open'));
-      if (!isOpen) item.classList.add('open');
-    });
-});
